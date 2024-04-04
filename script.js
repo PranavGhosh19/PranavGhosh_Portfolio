5b3ef34993128ea6173c7f933a1e425065c6bd6e
Pranav Ghosh <112719362+PranavGhosh19@users.noreply.github.com>
2024-04-05 00:06:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+        /* ----- TYPING EFFECT ----- */
+        var typed = new Typed(".auto-type", {
+            strings:["Data Analyst.", "Business Analyst.", "Business Intelligence.", "Data Scientist."],
+            typeSpeed:50,
+            backSpeed:50,
+            loop:true
+        });
