23b09bfd52077a70b7dceee17a0bca4c384b9945
Pranav Ghosh <112719362+PranavGhosh19@users.noreply.github.com>
2024-04-15 02:16:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
         /* ----- TYPING EFFECT ----- */
         var typed = new Typed(".auto-type", {
-            strings:["Data Analyst.", "Business Analyst.", "Business Intelligence.", "Data Scientist."],
+            strings:["Data Analyst.", "Business Analyst.", "Business Intelligence Analyst.", "Data Scientist."],
             typeSpeed:50,
             backSpeed:50,
             loop:true
